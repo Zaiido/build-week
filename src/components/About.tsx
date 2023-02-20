ce8b146fdd82d22bc3b5361ef62737369909742f
ajeer333 <ajeer333@gmail.com>
2023-02-21 00:39:15 +0100

    commit

---
+++ src/components/About.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+import{Container, Row, Col} from 'react-bootstrap'
+import { Pencil } from "react-bootstrap-icons";
+const About = () => {
+
+    return(
+        <Container  >
+    <Row >
+      <Col xs={9} className="mt-2 sub-sections">
+        <div className="d-flex justify-content-between">
+      <h4 className="name pt-4 mb-n1 px-2">About</h4>
+      <span className="mt-2"><Pencil/></span>
+      </div>
+      <p className="ml-2 mt-3 about">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labxcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+     
+      </Col>
+    </Row>
+  </Container>
+  
+    )
+
+};
+export default About;
---
+++ src/interfaces/IProfile.ts
@@ -0,0 +1,14 @@
+export interface IProfile {
+  _id: string;
+  name: string;
+  surname: string;
+  email: string;
+  username: string;
+  title: string;
+  bio: string;
+  area: string;
+  image: string;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+}
