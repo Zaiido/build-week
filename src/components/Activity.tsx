9a3d23a81c04c3311c0a6c46dcbddcc12b91be9f
Zaide Kurti <zaiido1920@gmail.com>
2023-02-21 23:40:57 +0100

    Update navbars

--- src/components/Activity.tsx
+++ src/components/Activity.tsx
@@ -23,7 +23,7 @@ const Activity = () => {
             </Button>
           </div>
           <h6 style={{ fontWeight: "600", paddingInline: "23px" }}>
-            {"You have'nt posted lately"}
+            You haven't posted lately
           </h6>
           <p className="about" style={{ paddingInline: "23px" }}>
             Recent posts you share or comment on will be displayed here
