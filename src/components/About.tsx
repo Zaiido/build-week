3b7973f6385b81d99c7542357dca81e2962b352b
ajeer333 <ajeer333@gmail.com>
2023-02-22 13:31:34 +0100

    commit

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,30 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Modal, Button, Form } from "react-bootstrap";
 import { Pencil } from "react-bootstrap-icons";
-import { useAppSelector } from "../hooks/hooks";
+import { useAppSelector, useAppDispatch } from "../hooks/hooks";
+import { useState } from "react";
+import { editBioAction } from "../actions";
 const About = () => {
+  const [show, setShow] = useState(false);
+  const [about, setAbout] = useState({
+    bio: "",
+  });
   let prof = useAppSelector((state) => state.myProfile.results);
 
+  const handleClose = () => setShow(false);
+  const handleShow = () => {
+    setShow(true);
+    setAbout(prof);
+  };
+  const dispatch = useAppDispatch();
+
+  const handleSubmit = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    e.preventDefault();
+
+    dispatch(editBioAction(about));
+    // eslint-disable-next-line no-restricted-globals
+    location.reload();
+  };
   return (
     <Container>
       <Row>
@@ -12,7 +32,46 @@ const About = () => {
           <div className="d-flex justify-content-between">
             <h4 className="name pt-4 mb-n1 px-2">About</h4>
             <span className="mt-2">
-              <Pencil />
+              <Pencil onClick={handleShow} />
+              <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Edit about</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <Form.Group controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>
+                      {" "}
+                      You can write about your years of experience, industry, or
+                      skills. People also talk about their achievements or
+                      previous job experiences.
+                    </Form.Label>
+                    <Form.Control
+                      as="textarea"
+                      rows={5}
+                      value={about.bio}
+                      onChange={(e) => {
+                        setAbout({
+                          ...about,
+                          bio: e.target.value,
+                        });
+                      }}
+                    />
+                  </Form.Group>
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={handleClose}>
+                    Close
+                  </Button>
+                  <Button
+                    variant="primary"
+                    onClick={(e) => {
+                      handleSubmit(e);
+                    }}
+                  >
+                    Save Changes
+                  </Button>
+                </Modal.Footer>
+              </Modal>
             </span>
           </div>
           <p className="ml-2 mt-3 about">{prof.bio}</p>
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -92,7 +92,6 @@ const Experience = () => {
     <Container className="mb-2">
       <Row>
         <Col xs={9} className="mt-2 sub-sections">
-          {/* <h4 className="name pt-4 mb-n1 px-2">Experience</h4> */}
           <div className="d-flex justify-content-between align-items-center">
             <h4 className="name pt-4 mb-n1 px-2">Experience</h4>
             <div>
@@ -389,3 +388,39 @@ const Experience = () => {
   );
 };
 export default Experience;
+
+//     <div
+//       className="d-flex justify-content-between align-items-center"
+//       style={{ paddingInline: "15px" }}
+//     >
+//       <div>
+//         <h4 className="name pt-4 mb-n1 px-2">Activity</h4>
+//         <p className="connections mt-2 mb-1 px-2 ">
+//           <a href="#home" className="link-connections">
+//             494 Followers
+//           </a>
+//         </p>
+//       </div>
+//       <Button variant="outline-primary" className="rounded-pill post">
+//         Start a post
+//       </Button>
+//     </div>
+//     <h6 style={{ fontWeight: "600", paddingInline: "23px" }}>
+//       You haven't posted lately
+//     </h6>
+//     <p className="about" style={{ paddingInline: "23px" }}>
+//       Recent posts you share or comment on will be displayed here
+//     </p>
+//     <ListGroup>
+//       <ListGroup.Item className="activity-item">
+//         <div className="text-center">
+//           <a href="#home">
+//             Show all activity
+//             <ArrowRight className="ml-2" />
+//           </a>
+//         </div>
+//       </ListGroup.Item>
+//     </ListGroup>
+//   </Col>
+// </Row>
+// </Container>
