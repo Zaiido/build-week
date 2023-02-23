c47d1d3fa4e1cefbc0cf0974b2adb6a707922d03
Zaide Kurti <zaiido1920@gmail.com>
2023-02-23 11:19:00 +0100

    Updated post styles

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -12,6 +12,7 @@ import { fetchPostsAction } from "../actions";
 import { useAppSelector, useAppDispatch } from "../hooks/hooks";
 
 import moment from "moment";
+import { Link } from "react-router-dom";
 
 const PostCard = () => {
   let prof = useAppSelector((state) => state.myProfile.results);
@@ -34,47 +35,47 @@ const PostCard = () => {
           .reverse()
           .map((singlePost) => (
             <Col className="mt-3 sub-sections" xs={12} key={singlePost._id}>
-              <div className="d-flex justify-content-between mt-2">
-                <div className="d-flex" style={{ gap: "5px" }}>
-                  <img
-                    src={singlePost.user.image}
-                    alt="vh"
-                    height="40px"
-                    width="40px"
-                    className="mt-n4"
-                  />
+              <div className="d-flex justify-content-between mt-3">
+                <div className="d-flex">
+                  <div className="image-container align-self-start">
+                    <img
+                      src={singlePost.user.image}
+                      alt="vh"
+                    />
+                  </div>
                   <div>
-                    <span
+                    <Link to={"/"}
+                      className="post-profile-link"
                       style={{
                         lineHeight: "24px",
                         fontWeight: "600",
-                        color: "rgba(0, 0, 0, 0.9)",
                         fontSize: "16px",
                       }}
                     >
                       {singlePost.username}
-                    </span>
-                    <p className="place mb-n1">{singlePost.user.title}</p>
+                    </Link>
+                    <p style={{ fontSize: "12px" }} className="place mb-n1">{singlePost.user.title}</p>
 
-                    <p className="place">
+                    <p className="place" style={{ fontSize: "12px" }}>
                       {moment(singlePost.createdAt).fromNow()}
                     </p>
                   </div>
                 </div>
 
                 {prof._id === singlePost.user._id ? (
-                  <Dropdown className="drop-down mt-n5 mr-n3">
+                  <Dropdown className="drop-down align-self-start">
                     <Dropdown.Toggle
                       variant="secondary"
                       id="dropdown-basic"
                       size="sm"
+                      className="special-dropdown icons-bg-hover"
                     >
                       <ThreeDots color="black" />
                     </Dropdown.Toggle>
 
-                    <Dropdown.Menu>
-                      <Dropdown.Item href="#/action-1">Edit post</Dropdown.Item>
-                      <Dropdown.Item href="#/action-2">
+                    <Dropdown.Menu className="special-dropdown-menu">
+                      <Dropdown.Item href="#/action-1" style={{ fontWeight: "100", lineHeight: "2" }}>Edit post</Dropdown.Item>
+                      <Dropdown.Item href="#/action-2" style={{ fontWeight: "100", lineHeight: "2" }}>
                         Delete post
                       </Dropdown.Item>
                     </Dropdown.Menu>
@@ -84,17 +85,17 @@ const PostCard = () => {
                 )}
               </div>
               <p className="about">{singlePost.text}</p>
-              <hr />
-              <div className="d-flex justify-content-between mb-2">
-                <div className="about">
+              <hr className="mb-1" />
+              <div className="d-flex justify-content-between mb-1">
+                <div className="about about-btn p-3">
                   <HandThumbsUp className="mr-1" />
                   Like
                 </div>
-                <div className="about">
+                <div className="about about-btn p-3">
                   {" "}
                   <ChatRightText className="mr-1" /> Comment
                 </div>
-                <div className="about">
+                <div className="about about-btn p-3">
                   {" "}
                   <Share className="mr-1" /> Share{" "}
                 </div>
